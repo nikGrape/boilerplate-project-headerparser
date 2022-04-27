359195b24c63c2dfe6d9cb9823f1968dacb6f49b
Nikolai Vinogradov <nik@Nikolais-MacBook-Pro.local>
2022-04-28 00:02:17 +0300

    done

--- server.js
+++ server.js
@@ -3,8 +3,12 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const { detect } = require('detect-browser');
+const browser = detect();
+const { networkInterfaces } = require('os');
+const nets = networkInterfaces();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -23,6 +27,43 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+
+app.get("/api/whoami", (req, res) => {
+  console.log(process.env.USER);
+  console.log(process.env.LANG);
+// handle the case where we don't detect the browser
+  if (browser) {
+    console.log("name: ", browser.name);
+    console.log("version: ", browser.version);
+    console.log("os: ", browser.os);
+    console.log(browser);
+  }
+  let results = {};
+  if (nets) {
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+          if (!results[name]) {
+            results[name] = [];
+          }
+          results[name].push(net.address);
+        }
+      }
+    }
+    console.log(nets.en0[1].address);
+  }
+
+  
+
+  res.json({
+    "ipaddress" : Object.values(results).flat()[0],
+    "language": process.env.LANG,
+    "software": browser ? `${browser.name} ${browser.version} ${browser.os}` : 'browser info'
+  })
+  
 });
 
 
